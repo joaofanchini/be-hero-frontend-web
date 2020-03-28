806c03b37fac73e30697fc8ef9ef078bffd466e7
João Fanchini <joaofanchini@gmail.com>
2020-03-28 00:03:12 -0300

    Retornando incidentes cadastrados

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //Para importar estilo é necessário subir dnovo
 import './style.css';
@@ -10,18 +10,50 @@ import { FiLogIn } from 'react-icons/fi';
 
 // Objeto necessário para ter o comportamento de SPA, que é o de não recarregar todo o conteúdo, e sim só o necessário do virtual
 // No comportamento de SPA, ele não recarrega a página, apenas subtitui os elementos da mesma
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api';
 
 // No React, devemos usar a propriedade class name para colcoar classe nos elementos
 
 export default function Logon() {
+  const history = useHistory();
+  const [idOng, setIdOng] = useState('');
+
+  async function logOn(event) {
+    // Lembrar que o evento sempre é passado como parâmetro
+    event.preventDefault();
+
+    let data = {
+      id: idOng
+    };
+    try {
+      let response = await api.post('session', data);
+
+      //Dispondo isso na aplicação inteira através da aplicação inteira
+      sessionStorage.setItem('idOng', data.id); //Salvando no sessionStorage
+      localStorage.setItem('nameOng', response.data.ong.name); // Salvando no LocalStorage
+
+      console.log(`Ong logada: ${response.data.name}`);
+
+      history.push('/profile');
+    } catch (error) {
+      console.log(error);
+      alert('Falha no login');
+    }
+  }
+
   return (
     <div className='logon-container'>
       <section className='form'>
         <img src={branchImage} alt='imagem de logo' />
-        <form>
+        <form onSubmit={logOn}>
           <h1>Faça seu logon</h1>
-          <input type='text' placeholder='Sua ID' />
+          <input
+            type='text'
+            value={idOng}
+            onChange={e => setIdOng(e.target.value)}
+            placeholder='Sua ID'
+          />
           <button className='button' type='submit'>
             Entrar
           </button>
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,16 +1,51 @@
-import React, { Profiler } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 import { Link } from 'react-router-dom';
 import branchImage from '../../assets/logo.svg';
 
 import { FiPower, FiTrash2 } from 'react-icons/fi';
+import api from '../../services/api';
 
 export default function Profile() {
+  const nameOng = localStorage.getItem('nameOng');
+  const idOng = sessionStorage.getItem('idOng');
+
+  const [incident, setIncident] = useState([]); //Aqui meu setState começa com uma lista
+
+  useEffect(() => {
+    // use Effects nada mais executa uma função ao recarregar a tela, além de recarregar a função para cada mudança na variável indicada
+    api
+      .get('/profile', {
+        headers: {
+          Authorization: idOng
+        }
+      })
+      .then(response => {
+        setIncident(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+        alert('Erro ao efetuar consulta de perfis');
+      });
+  }, idOng);
+
+  /**
+   * Sempre que for retornar uma JSX, é necessário abrir os () e colocar o return,
+   * mesmo que seja dentro de uma outra JSX.
+   *
+   * Lembrar também que sempre que quisermos colocar uma função javascript no meio do
+   * código, é necessário abrir {}
+   *
+   * PS: Colocar como return () de uma função, já implica que em 'return ();'
+   *
+   * PS: Com React, sempre que for fazer uma listagem, é necessário adicionar o identificador
+   * único à aquela linha (li), e com React, fazemos isso através do atributo 'key'
+   */
   return (
     <div className='profile-container'>
       <header>
-        <img src={branchImage} alt='Logo image' />
-        <span>Bem vindo ONG</span>
+        <img src={branchImage} alt='Logo' />
+        <span>Bem vindo, {nameOng}</span>
 
         <Link className='button' to='/new-incident'>
           Cadastrar novo caso
@@ -23,35 +58,22 @@ export default function Profile() {
 
       <h1>Casos cadastrados</h1>
       <ul>
-        <li>
-          <strong>Caso:</strong>
-          <p>Caso teste</p>
+        {incident.map(i => (
+          <li key={i.id}>
+            <strong>Caso:</strong>
+            <p>{i.title}</p>
 
-          <strong>Descrição:</strong>
-          <p>descrição teste</p>
+            <strong>Descrição:</strong>
+            <p>{i.description}</p>
 
-          <strong>Valor:</strong>
-          <p>R$120,00</p>
+            <strong>Valor:</strong>
+            <p>{i.value}</p>
 
-          <button type='button'>
-            <FiTrash2 size={18} color='#a8a8b3' />
-          </button>
-        </li>
-
-        <li>
-          <strong>Caso:</strong>
-          <p>Caso teste</p>
-
-          <strong>Descrição:</strong>
-          <p>descrição teste</p>
-
-          <strong>Valor:</strong>
-          <p>R$120,00</p>
-
-          <button type='button'>
-            <FiTrash2 size={18} color='#a8a8b3' />
-          </button>
-        </li>
+            <button type='button'>
+              <FiTrash2 size={18} color='#a8a8b3' />
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
